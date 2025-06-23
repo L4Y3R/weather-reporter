d99b8a7300cd5cbe2965f91fca9d29f1090f6969
Nipun Balage <105732109+L4Y3R@users.noreply.github.com>
2025-06-23 15:13:32 +0530

    slight chnage to clear sky

--- src/app/weather-client.tsx
+++ src/app/weather-client.tsx
@@ -18,7 +18,7 @@ export function WeatherClient() {
   const [error, setError] = useState(false);
 
   const weatherGradients: Record<string, string> = {
-    Clear: "bg-gradient-to-tr from-sky-900 via-sky-800 to-sky-700",          
+    Clear: "bg-gradient-to-tr from-yellow-400 via-sky-800 to-sky-700",          
     Sunny: "bg-gradient-to-tr from-yellow-800 via-orange-700 to-amber-600",    
     Cloudy: "bg-gradient-to-tr from-gray-800 via-gray-700 to-gray-600",       
     Rain: "bg-gradient-to-tr from-blue-950 via-gray-800 to-gray-700",           
